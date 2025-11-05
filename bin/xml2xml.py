e310e0de585ec07215d37069ae88b55f10c6f8b3
Filipe Vasconcelos <filipe.manuel.vasconcelos@gmail.com>
2025-11-05 16:19:18 +0100

    possibilité de choisir entre deux engines de traduction --google_cloud --libretranslate

--- bin/xml2xml.py
+++ bin/xml2xml.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
 import os
 import sys
 import html
+import requests
 import argparse
 from lxml import etree
 #--------------------------------------------------------------------------------------------------
@@ -25,10 +27,28 @@ tags_a_traduire_par_type={ "coderunner"  : question_tags + general_feedback,
                            "shortanswer" : question_tags + general_feedback + partial_feedbacks,
                            "numerical"   : question_tags + general_feedback,
                            "category"    : category_tags }
-#--------------------------------------------------------------------------------------------------
-def translate_text(target: str, text: str) -> dict:
-    """Translates text into the target language.
 
+#--------------------------------------------------------------------------------------------------
+# choisir parmi les 'engines' disponible pour la traduction
+def translate_text(target,text,engine):
+    engine = engine or "google_cloud"
+    match engine :
+        case "google_cloud" :
+            return translate_text_google_cloud(target,text)
+        case "libretranslate" : 
+            return translate_text_libretranslate(target,text)
+#--------------------------------------------------------------------------------------------------
+def translate_text_libretranslate(target,text,url="http://localhost:5000/translate"):
+    data = {
+    "q": text,
+    "source": "fr",
+    "target": target}
+    response = requests.post(url, data=data)
+    response.raise_for_status()
+    return response.json()["translatedText"]
+#--------------------------------------------------------------------------------------------------
+def translate_text_google_cloud(target: str, text: str) -> dict:
+    """Translates text into the target language.
     Target must be an ISO 639-1 language code.
     See https://g.co/cloud/translate/v2/translate-reference#supported_languages
     """
@@ -38,26 +58,8 @@ def translate_text(target: str, text: str) -> dict:
         text = text.decode("utf-8")
     result = translate_client.translate(text, target_language=target)
     return html.unescape(result["translatedText"])
-
 #--------------------------------------------------------------------------------------------------
-def dir_path(path):
-    return path if os.path.isdir(path) else argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
-#--------------------------------------------------------------------------------------------------
-def parsing_command_line():
-    import os
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i','--input', nargs='+', type=argparse.FileType('r'),
-                        default=sys.stdin,help='input files (on single or a set)',required=True)
-    parser.add_argument('-o','--outpath', nargs='?', type=dir_path,
-                        default='./', help='output path')
-    args = parser.parse_args()
-
-    outpath=args.outpath
-    path=os.path.dirname(args.input[0].name)+'/'
-    filespath=args.input
-    return path,filespath,outpath,"en"
-
-def translate_xml(file,target="en"):
+def translate_xml(file,target,outpath,engine):
     fileout=outpath+os.path.basename(file.name).replace(".xml","_en.xml")
     print(f"{file.name} -> {fileout}",file=sys.stderr)
     tree = etree.parse(file, parser)
@@ -69,15 +71,33 @@ def translate_xml(file,target="en"):
         for tag in tags_a_traduire_par_type[qtype] : 
             for t in root.xpath(tag):
                 if t.text:
-                    translated=translate_text(target,t.text)
+                    translated=translate_text(target,t.text,engine)
                     t.text = etree.CDATA(translated) if tag in with_cdata else translated
     tree.write(fileout, encoding="UTF-8", xml_declaration=True, pretty_print=False)
-
+#--------------------------------------------------------------------------------------------------
+def dir_path(path):
+    return path if os.path.isdir(path) else argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
+#--------------------------------------------------------------------------------------------------
+def parsing_command_line():
+    import os
+    ENGINES=["google_cloud","libretranslate"]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i','--input', nargs='+', type=argparse.FileType('r'),
+                        default=sys.stdin,help='input files (on single or a set)',required=True)
+    parser.add_argument('-o','--outpath', nargs='?', type=dir_path, default='./', help='output path')
+    parser.add_argument('-g','--google_cloud',   action='store_true', default=False, 
+                         help='utiliser l\'api translate de google-cloud')
+    parser.add_argument('-l','--libretranslate', action='store_true', default=False,
+                         help='utiliser l\'api de libretranslate')
+    parser.add_argument('-t','--target', nargs="?", default="en", help='langue cible de la traduction')
+    args = parser.parse_args()
+    engine = next((name for name in ENGINES if getattr(args, name)), None)
+    setattr(args, "engine", engine)
+    #print(args)
+    return args
 #--------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
-    path,filespath,outpath,target = parsing_command_line()
-
+    args = parsing_command_line()
     parser = etree.XMLParser(strip_cdata=False, remove_comments=False)
-
-    for file in filespath :
-        translate_xml(file,target=target)
+    for file in args.input :
+        translate_xml(file,target=args.target,outpath=args.outpath,engine=args.engine)
